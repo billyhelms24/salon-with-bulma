32d9b952afb0dff76ba192e3c41b9c3f1c499545
billyhelms24 <williamtvhelms@gmail.com>
2021-02-22 15:47:36 -0500

    Add services.html and content

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,29 +1,102 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
-});
+document.addEventListener("DOMContentLoaded", () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll(".navbar-burger"),
+        0
+    );
 
-document.addEventListener('DOMContentLoaded', () => {
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach((el) => {
+            el.addEventListener("click", () => {
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle("is-active");
+                $target.classList.toggle("is-active");
+            });
+        });
+    }
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
+    const showCutModalButton = document
+        .getElementById("showCutModal")
+        .addEventListener("click", function () {
+            document.getElementById("cutModal").classList.add("is-active");
+        });
+    const hideCutModalButton = document
+        .getElementById("hideCutModal")
+        .addEventListener("click", function () {
+            document.getElementById("cutModal").classList.remove("is-active");
+        });
 
-    // Add a click event on each of them
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
+    const showColorModalButton = document
+        .getElementById("showColorModal")
+        .addEventListener("click", function () {
+            document.getElementById("colorModal").classList.add("is-active");
+        });
+    const hideColorModalButton = document
+        .getElementById("hideColorModal")
+        .addEventListener("click", function () {
+            document.getElementById("colorModal").classList.remove("is-active");
+        });
 
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
+    const showAddColorModalButton = document
+        .getElementById("showAddColorModal")
+        .addEventListener("click", function () {
+            document.getElementById("addColorModal").classList.add("is-active");
+        });
+    const hideAddColorModalButton = document
+        .getElementById("hideAddColorModal")
+        .addEventListener("click", function () {
+            document
+                .getElementById("addColorModal")
+                .classList.remove("is-active");
+        });
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+    const showStyleModalButton = document
+        .getElementById("showStyleModal")
+        .addEventListener("click", function () {
+            document.getElementById("styleModal").classList.add("is-active");
+        });
+    const hideStyleModalButton = document
+        .getElementById("hideStyleModal")
+        .addEventListener("click", function () {
+            document.getElementById("styleModal").classList.remove("is-active");
+        });
 
-      });
-    });
-  }
+    const showPermModalButton = document
+        .getElementById("showPermModal")
+        .addEventListener("click", function () {
+            document.getElementById("permModal").classList.add("is-active");
+        });
+    const hidePermModalButton = document
+        .getElementById("hidePermModal")
+        .addEventListener("click", function () {
+            document.getElementById("permModal").classList.remove("is-active");
+        });
 
+    const showBBModalButton = document
+        .getElementById("showBBModal")
+        .addEventListener("click", function () {
+            document.getElementById("bbModal").classList.add("is-active");
+        });
+    const hideBBModalButton = document
+        .getElementById("hideBBModal")
+        .addEventListener("click", function () {
+            document.getElementById("bbModal").classList.remove("is-active");
+        });
+
+    const showWaxModalButton = document
+        .getElementById("showWaxModal")
+        .addEventListener("click", function () {
+            document.getElementById("waxModal").classList.add("is-active");
+        });
+    const hideWaxModalButton = document
+        .getElementById("hideWaxModal")
+        .addEventListener("click", function () {
+            document.getElementById("waxModal").classList.remove("is-active");
+        });
 });
