dec85b67462377f41257dc32104c916f0d75a729
billyhelms24 <williamtvhelms@gmail.com>
2021-08-26 11:17:16 -0400

    Adds notifcation regarding remodel

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,19 +1,3 @@
-// function resizeServiceSquareHeights() {
-//     document.querySelectorAll(".service-square").forEach((el) => {
-//         el.style.height = el.offsetWidth + "px";
-//         el.style.lineHeight = el.offsetWidth + "px";
-//     });
-// }
-
-// document.querySelectorAll(".service-square-text").forEach((el) => {
-//     el.addEventListener("mouseover", (event) => {
-//         event.target.style.backgroundColor = "rgba(203, 153, 126, .8)";
-//     });
-//     el.addEventListener("mouseout", (event) => {
-//         event.target.style.backgroundColor = "";
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(
@@ -45,86 +29,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    /* resizeServiceSquareHeights();
+    (document.querySelectorAll(".notification .delete") || []).forEach(
+        ($delete) => {
+            const $notification = $delete.parentNode;
 
-    const showCutModalButton = document
-        .getElementById("showCutModal")
-        .addEventListener("click", function () {
-            document.getElementById("cutModal").classList.add("is-active");
-        });
-    const hideCutModalButton = document
-        .getElementById("hideCutModal")
-        .addEventListener("click", function () {
-            document.getElementById("cutModal").classList.remove("is-active");
-        });
-
-    const showColorModalButton = document
-        .getElementById("showColorModal")
-        .addEventListener("click", function () {
-            document.getElementById("colorModal").classList.add("is-active");
-        });
-    const hideColorModalButton = document
-        .getElementById("hideColorModal")
-        .addEventListener("click", function () {
-            document.getElementById("colorModal").classList.remove("is-active");
-        });
-
-    const showTreatmentsModalButton = document
-        .getElementById("showTreatmentsModal")
-        .addEventListener("click", function () {
-            document
-                .getElementById("treatmentsModal")
-                .classList.add("is-active");
-        });
-    const hideTreatmentsModalButton = document
-        .getElementById("hideTreatmentsModal")
-        .addEventListener("click", function () {
-            document
-                .getElementById("treatmentsModal")
-                .classList.remove("is-active");
-        });
-
-    const showStyleModalButton = document
-        .getElementById("showStyleModal")
-        .addEventListener("click", function () {
-            document.getElementById("styleModal").classList.add("is-active");
-        });
-    const hideStyleModalButton = document
-        .getElementById("hideStyleModal")
-        .addEventListener("click", function () {
-            document.getElementById("styleModal").classList.remove("is-active");
-        });
-
-    const showPermModalButton = document
-        .getElementById("showPermModal")
-        .addEventListener("click", function () {
-            document.getElementById("permModal").classList.add("is-active");
-        });
-    const hidePermModalButton = document
-        .getElementById("hidePermModal")
-        .addEventListener("click", function () {
-            document.getElementById("permModal").classList.remove("is-active");
-        });
-
-    const showBBModalButton = document
-        .getElementById("showBBModal")
-        .addEventListener("click", function () {
-            document.getElementById("bbModal").classList.add("is-active");
-        });
-    const hideBBModalButton = document
-        .getElementById("hideBBModal")
-        .addEventListener("click", function () {
-            document.getElementById("bbModal").classList.remove("is-active");
-        });
-
-    const showWaxModalButton = document
-        .getElementById("showWaxModal")
-        .addEventListener("click", function () {
-            document.getElementById("waxModal").classList.add("is-active");
-        });
-    const hideWaxModalButton = document
-        .getElementById("hideWaxModal")
-        .addEventListener("click", function () {
-            document.getElementById("waxModal").classList.remove("is-active");
-        }); */
+            $delete.addEventListener("click", () => {
+                $notification.parentNode.removeChild($notification);
+            });
+        }
+    );
 });
